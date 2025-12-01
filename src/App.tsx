3125142308ab4b40f5fd7c553a23fc077ce407df
Enkhbayar <enkhe.tech@gmail.com>
2025-12-01 12:59:28 +0800

    basic dark layer added to map with maptiler and skeleton loader with shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import HourlyForecast from "./components/cards/HourlyForecast";
 import CurrentWeather from "./components/cards/CurrentWeather";
 import AdditionalInfo from "./components/cards/AdditionalInfo";
 import Map from "./components/Map";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import type { Coords } from "./types";
 import LocationDropdown from "./components/dropdowns/LocationDropdown";
 import { useQuery } from "@tanstack/react-query";
 import { fetchCityCoordinates } from "./api";
+import MapTypeDropdown from "./components/dropdowns/MapTypeDropdown";
+import MapLegend from "./components/MapLegend";
+import CurrentSkeleton from "./components/skeletons/CurrentSkeleton";
+import HourlySkeleton from "./components/skeletons/HourlySkeleton";
+import DailySkeleton from "./components/skeletons/DailySkeleton";
+import AdditionalInfoSkeleton from "./components/skeletons/AdditionalInfoSkeleton";
 
 function App() {
   const [coordinates, setCoordinates] = useState<Coords>({
@@ -15,6 +21,7 @@ function App() {
     lon: 45,
   });
   const [city, setCity] = useState("Ulaanbaatar");
+  const [mapType, setMapType] = useState("clouds_new");
 
   const { data: geoCodeData } = useQuery({
     queryKey: ["geocode", city],
@@ -33,12 +40,32 @@ function App() {
 
   return (
     <div className="flex flex-col gap-8">
-      <LocationDropdown city={city} setCity={setCity} />
-      <Map coords={coords} onMapClick={onMapClick} />
-      <CurrentWeather coords={coords} />
-      <HourlyForecast coords={coords} />
-      <DailyForecast coords={coords} />
-      <AdditionalInfo coords={coords} />
+      <div className="flex gap-8">
+        <div className="flex gap-4">
+          <h1 className="text-xl font-semibold">Location: </h1>
+          <LocationDropdown city={city} setCity={setCity} />
+        </div>
+        <div className="flex gap-4">
+          <h1 className="text-xl font-semibold">Map Type: </h1>
+          <MapTypeDropdown mapType={mapType} setMapType={setMapType} />
+        </div>
+      </div>
+      <div className="relative">
+        <Map coords={coords} onMapClick={onMapClick} mapType={mapType} />
+        <MapLegend mapType={mapType} />
+      </div>
+      <Suspense fallback={<CurrentSkeleton />}>
+        <CurrentWeather coords={coords} />
+      </Suspense>
+      <Suspense fallback={<HourlySkeleton />}>
+        <HourlyForecast coords={coords} />
+      </Suspense>
+      <Suspense fallback={<DailySkeleton />}>
+        <DailyForecast coords={coords} />
+      </Suspense>
+      <Suspense fallback={<AdditionalInfoSkeleton />}>
+        <AdditionalInfo coords={coords} />
+      </Suspense>
     </div>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,18 @@
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import type { Coords } from "../types";
+import { useEffect } from "react";
+import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 type Props = {
   coords: Coords;
   onMapClick: (lat: number, lon: number) => void;
+  mapType: string;
 };
 
-export default function Map({ coords, onMapClick }: Props) {
+export default function Map({ coords, onMapClick, mapType }: Props) {
   const { lat, lon } = coords;
   return (
     <MapContainer
@@ -18,9 +23,14 @@ export default function Map({ coords, onMapClick }: Props) {
       scrollWheelZoom={false}
     >
       <MapClick onMapClick={onMapClick} coords={coords} />
-      <TileLayer
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /> */}
+      <MapTileLayer />
+      <TileLayer
+        opacity={0.8}
+        url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
       />
       <Marker position={[lat, lon]} />
     </MapContainer>
@@ -41,5 +51,22 @@ function MapClick({
     const { lat, lng } = e.latlng;
     onMapClick(lat, lng);
   });
+  return null;
+}
+
+function MapTileLayer() {
+  const map = useMap();
+  useEffect(() => {
+    const tileLayer = new MaptilerLayer({
+      style: "basic-dark",
+      apiKey: "QRdyAmSF2oS6tKdtAFrc",
+    });
+    tileLayer.addTo(map);
+
+    return () => {
+      map.removeLayer(tileLayer);
+    };
+  }, [map]);
+
   return null;
 }
