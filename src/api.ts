1040f74c8036d752a171c00868b58feae428130b
Enkhbayar <enkhe.tech@gmail.com>
2026-04-30 16:06:53 +0800

    improvement: improved api calls

--- src/api.ts
+++ src/api.ts
@@ -2,28 +2,16 @@ import { GeocodeSchema } from "./schemas/geocodeSchema";
 import { WeatherSchema } from "./schemas/weatherSchema";
 import { PollutionSchema } from "./schemas/PollutionSchema";
 
-async function getApiKey() {
-  const API_KEY = await fetch("https://openweather-api-21sx.onrender.com").then(
-    (res) => res.text(),
-  );
-  return API_KEY;
-}
-
-const API_KEY = await getApiKey();
+const BASE = "https://openweather-api-21sx.onrender.com";
 
 export async function fetchData({ lat, lon }: { lat: number; lon: number }) {
-  console.log("this is the api key", API_KEY);
-  const response = await fetch(
-    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${API_KEY}`,
-  );
+  const response = await fetch(`${BASE}/weather?lat=${lat}&lon=${lon}`);
   const data = await response.json();
   return WeatherSchema.parse(data);
 }
 
 export async function fetchCityCoordinates(city: string) {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`,
-  );
+  const response = await fetch(`${BASE}/geocode?city=${city}`);
   const data = await response.json();
   return GeocodeSchema.parse(data);
 }
@@ -35,9 +23,7 @@ export async function getAirPollution({
   lat: number;
   lon: number;
 }) {
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
-  );
+  const response = await fetch(`${BASE}/pollution?lat=${lat}&lon=${lon}`);
   const data = await response.json();
   return PollutionSchema.parse(data);
 }
